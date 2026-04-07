09d1e890460392a0ecced7e6ab6e01708ec04d04
Federica Iola Caselli <160833909+fedecas123@users.noreply.github.com>
2026-04-07 17:51:13 +0200

    part 2 assignment

--- RLI_17_A0/gym_race/envs/race_env.py
+++ RLI_17_A0/gym_race/envs/race_env.py
@@ -49,3 +49,58 @@ class RaceEnv(gym.Env):
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
+
+
+class RaceEnvV3(gym.Env):
+    """
+    Pyrace-v3 improvements over v1:
+    Observations: 6 continuous floats in [0,1]
+    Actions: 4 (ACCELERATE, TURN_LEFT, TURN_RIGHT, BRAKE)
+    Reward: Dense reward function (progress + speed bonus + checkpoint/goal milestones)
+    """
+    metadata = {'render_modes': ['human'], 'render_fps': 30}
+
+    def __init__(self, render_mode="human"):
+        print("init RaceEnvV3")
+        # 4 actions
+        self.action_space = spaces.Discrete(4)
+        # 5 radar readings + 1 speed, all normalised to [0, 1]
+        self.observation_space = spaces.Box(low=np.zeros(6, dtype=np.float32), high=np.ones(6, dtype=np.float32), dtype=np.float32,)
+        self.is_view = True
+        self.pyrace = PyRace2D(self.is_view)
+        self.msgs = []
+        self.render_mode = render_mode
+
+    def reset(self, seed=None, options=None):
+        mode = self.pyrace.mode
+        del self.pyrace
+        self.is_view = True
+        self.msgs = []
+        self.pyrace = PyRace2D(self.is_view, mode=self.render_mode)
+        obs = self.pyrace.observe_v3()
+        return np.array(obs, dtype=np.float32), {}
+
+    def step(self, action):
+        self.pyrace.action_v3(action)
+        reward = self.pyrace.evaluate_v3()
+        done   = self.pyrace.is_done()
+        obs    = self.pyrace.observe_v3()
+        info   = {
+            'dist':  self.pyrace.car.distance,
+            'check': self.pyrace.car.current_check,
+            'crash': not self.pyrace.car.is_alive,
+        }
+        return np.array(obs, dtype=np.float32), reward, done, False, info
+
+    def render(self):
+        if self.is_view:
+            self.pyrace.view_(self.msgs)
+
+    def set_view(self, flag):
+        self.is_view = flag
+
+    def set_msgs(self, msgs):
+        self.msgs = msgs
+
+    def close(self):
+        pass
